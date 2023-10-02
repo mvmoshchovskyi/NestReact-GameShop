e08b24a7ed83ba1f288d03102c4c3ba9fc8eeee3
mmoshchovskyi <mykhailo.m@vivint.com>
2023-10-02 20:43:46 +0300

    setup React TS webpack

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export const App = () => {
+  return (
+    <>
+      <h1>
+        React type Script - {process.env.NODE_ENV} {process.env.name}
+      </h1>
+    </>
+  )
+}
